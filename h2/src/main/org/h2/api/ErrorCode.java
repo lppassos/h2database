33984d0649d3a7d9511ac5687b4d70a61792aa7c
Luis Passos <lpcarvalho2@gmail.com>
2021-05-12 15:15:49 +0100

    Include support to limit rows in a group by.

--- h2/src/main/org/h2/api/ErrorCode.java
+++ h2/src/main/org/h2/api/ErrorCode.java
@@ -2098,6 +2098,15 @@ public class ErrorCode {
     public static final int REMOTE_DATABASE_NOT_FOUND_1 = 90149;
 
     // next is 90150
+    /**
+     * The error with code <code>90147</code> is thrown when the in memory
+     * table containing all the entries of a group by statement would exceed
+     * the maximum capacity specified using the system property 
+     * <code>h2.maxGroupByEntries</code>.
+     */
+    public static final int GROUP_BY_TABLE_TOO_LARGE = 90147;
+
+    // next is 90148
 
     private ErrorCode() {
         // utility class
